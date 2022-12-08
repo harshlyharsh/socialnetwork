04d61f795e7b1f8fb479fb1a97bec44ffc8ca31f
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2022-12-08 23:36:42 +0530

    Basic Layout

---
+++ client/pages/_app.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "../components/Nav";
+import Head from "next/head";
+export default function MyApp({ Component, pageProps }) {
+    return (
+    <>
+    <Head>
+      <link rel="stylesheet" href="/css/style.css" />
+    </Head>
+<Nav />
+<Component {...pageProps} />
+    </>
+    )
+  } 
