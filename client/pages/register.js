19629679b99872da6c386a1a00fbd3886bea6ef3
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2022-12-10 10:41:24 +0530

    background image

--- client/pages/register.js
+++ client/pages/register.js
@@ -35,7 +35,7 @@ const Register = () => {
 
     return (
         <div className="container-fluid">
-            <div className="row py-5 bg-secondary text-light">
+            <div className="row py-5 bg-default-image ">
                 <div className="col text-center">
                   <h1>Register</h1>  
                 </div>
