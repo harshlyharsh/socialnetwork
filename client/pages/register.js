eb24a4b9f632afbc6b275465800bd7aff63f30c6
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2022-12-10 10:35:16 +0530

    loading state

--- client/pages/register.js
+++ client/pages/register.js
@@ -3,17 +3,20 @@ import axios from "axios";
 import {toast} from "react-toastify";
 import {Modal} from "antd";
 import Link from "next/Link";
+import {SyncOutlined} from "@ant-design/icons"
 const Register = () => {
     const[name, setName]=useState("");
     const[email, setEmail]=useState("");
     const[password, setPassword]=useState("");
     const[secret, setSecret]=useState("");
     const [ok,setOk] = useState(false);
+    const [loading, setLoading]=useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
      try{
    // console.log(name,email,password,secret);
+   setLoading(true);
    const {data} = await   axios.post(`${process.env.NEXT_PUBLIC_API}/register`,{
             name,email,password,secret,
         });
@@ -22,9 +25,11 @@ const Register = () => {
         setPassword('')
         setSecret('')
         setOk(data.ok);
+        setLoading(false);
      }
      catch(err){
         toast.error(err.response.data)
+        setLoading(false);
      }
     };
 
@@ -35,6 +40,9 @@ const Register = () => {
                   <h1>Register</h1>  
                 </div>
                 </div>
+
+                {loading ? <h1>Loading</h1> : ""}
+
                 <div className="row py-5">
                     <div className="col-md-6 offset-md-3">
                     <form onSubmit={handleSubmit}> <div className="form-group p-2">
@@ -66,7 +74,7 @@ const Register = () => {
                             type="text" className="form-control" placeholder="Write your answer here" />
                         </div>
                        <div className="form-group p-2">
-                       <button disabled={!name || !email || !password || !secret} className="btn btn-primary col-12">Submit</button>
+                       <button disabled={!name || !email || !password || !secret} className="btn btn-primary col-12">{loading ? <SyncOutlined spin className="py-1"/> : "Submit"}</button>
                        </div>
                         </form>
                 </div>
