88b99768a831f4f79e3197c2da648a147ceea448
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2022-12-23 20:21:08 +0530

    create_post_with_image

--- client/pages/user/dashboard.js
+++ client/pages/user/dashboard.js
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { UserContext } from "../../context";
+
 import CreatePostForm from "../../components/forms/CreatePostForm";
 import { useRouter, userRouter } from "next/router";
 import axios from "axios";
@@ -9,6 +10,9 @@ const Home = () => {
   const [state, setState] = useContext(UserContext);
   // state
   const [content, setContent] = useState("");
+  const [image, setImage] = useState({});
+  const [uploading, setUploading] = useState(false);
+
   // route
   const router = useRouter();
 
@@ -16,13 +20,14 @@ const Home = () => {
     e.preventDefault();
     // console.log("post => ", content);
     try {
-      const { data } = await axios.post("/create-post", { content });
+      const { data } = await axios.post("/create-post", { content, image });
       console.log("create post response => ", data);
       if (data.error) {
         toast.error(data.error);
       } else {
         toast.success("Post created");
         setContent("");
+        setImage({});
       }
     } catch (err) {
       console.log(err);
@@ -33,16 +38,24 @@ const Home = () => {
     const file = e.target.files[0];
     let formData = new FormData();
     formData.append("image", file);
-    console.log([...formData]);
+    // console.log([...formData]);
+    setUploading(true);
     try {
       const { data } = await axios.post("/upload-image", formData);
+      // console.log("uploaded image => ", data);
+      setImage({
+        url: data.url,
+        public_id: data.public_id,
+      });
+      setUploading(false);
     } catch (err) {
       console.log(err);
+      setUploading(false);
     }
   };
 
   return (
-
+  
       <div className="container-fluid">
         <div className="row py-5 text-light bg-default-image">
           <div className="col text-center">
@@ -56,12 +69,15 @@ const Home = () => {
               content={content}
               setContent={setContent}
               postSubmit={postSubmit}
+              handleImage={handleImage}
+              uploading={uploading}
+              image={image}
             />
           </div>
           <div className="col-md-4">Sidebar</div>
         </div>
       </div>
-  
+    
   );
 };
 
