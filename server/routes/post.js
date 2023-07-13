40a394638ebd9721c2d7ffa5f288b1ce9c9128f4
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-07-13 20:15:07 +0530

    home page

--- server/routes/post.js
+++ server/routes/post.js
@@ -19,6 +19,8 @@ import {
   addComment,
   removeComment,
   totalPosts,
+  posts,
+  getPost,
 } from "../controllers/post";
 
 router.post("/create-post", requireSignin, createPost);
@@ -48,5 +50,9 @@ router.put("/add-comment", requireSignin, addComment);
 router.put("/remove-comment", requireSignin, removeComment);
 
 router.get("/total-posts", totalPosts);
+
+router.get("/posts", posts);
+
+router.get("/post/:_id", getPost);
 
 module.exports = router;
